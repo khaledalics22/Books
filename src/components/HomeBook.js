917579aa6ca754c0a57e03b532e1070f3e2abedd
IslamAhmed1092 <eslam1092@hotmail.com>
2021-06-09 05:09:19 +0200

    add category to home book

--- src/components/HomeBook.js
+++ src/components/HomeBook.js
@@ -9,6 +9,8 @@ const HomeBook = ({book}) => {
             <div className='text-container'>
                 <h1 className='book-title'>{book.title}</h1>
                 <h4 className='book-author'>by {book.author_name}</h4>
+                <h5 className='category-label'>category:</h5>
+                <h5 className='book-category'>{book.category}</h5>
                 <h6 className='book-description'>{book.description}</h6>
             </div>
         </div>
